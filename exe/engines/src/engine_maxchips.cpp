7bc861f6fb2397479da6f65c54416389e2802327
Rasty3D <rasty3d@gmail.com>
2013-01-27 23:16:51 +0100

    The maxchips engine is now working

--- exe/engines/src/engine_maxchips.cpp
+++ exe/engines/src/engine_maxchips.cpp
@@ -15,9 +15,38 @@ extern "C" bool setParam(const char *name, const char *value)
 
 extern "C" bool move(Othello_board board, int turn, char *move)
 {
-	// TODO
-	move[0] = '4';
-	move[1] = 'f';
+	Othello_board boardAux;
+	int bestMoveScore = 0;
+	int score;
+
+	for (int row = 0; row < 8; row++)
+	{
+		for (int col = 0; col < 8; col++)
+		{
+			boardAux = board;
+
+			if (Othello_move(&boardAux, row, col, turn))
+			{
+				if (turn == OTHELLO_WHITE)
+					score = Othello_getWhiteCounter(&boardAux);
+				else
+					score = Othello_getBlackCounter(&boardAux);
+
+				if (bestMoveScore < score)
+				{
+					bestMoveScore = score;
+					move[0] = row + 'A';
+					move[1] = col + '1';
+				}
+			}
+		}
+	}
+
+	// Check if we could move
+	if (bestMoveScore == 0)
+		return false;
+
+	std::cout << move[0] << move[1] << std::endl;
 
 	return true;
 }
